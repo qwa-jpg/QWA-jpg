3240622bb8d9a02f72e77aaccd71f804cffaf2ce
qwa-jpg <a15026614973@outlook.com>
2026-09-05 18:10:58 +0800

    IDA-铁盘整理

--- 提高算法/tigao-luogu-p2534.cpp
+++ 提高算法/tigao-luogu-p2534.cpp
@@ -1,55 +1,51 @@
 #include<bits/stdc++.h>
-#define ll long long
 using namespace std;
-char ch1;
-template<class T>
-inline void rd(T& x){
-    x=0;bool w=0;
-    ch1=getchar();
-    while(!isdigit(ch1)){ch1=='-'&&(w=1),ch1=getchar();}
-    while(isdigit(ch1)){x=(x<<1)+(x<<3)+(ch1&15),ch1=getchar();}
-    w&&(x=(~x)+1);
-}
-template<class T>
-inline void wr(T x){
-    if(x<0)x=-x,putchar('-');
-    if(x<10){putchar(x+48);return;}
-    T L=x/10;
-    wr(L);
-    putchar(x-((L<<1)+(L<<3))+48);
-}
-int n,a[20],b[20],maxstep;
-bool d;
-int evaluate(){
+int n,a[25];
+int h(){
     int cnt=0;
     for(int i=1;i<=n;i++)if(abs(a[i]-a[i+1])!=1)cnt++;
     return cnt;
 }
-void dfs(int x,int eva,int step){
-    if(step==maxstep){
-        if(!eva)d=1;
-        return;
+bool dfs(int dep,int limit,int last){
+    int g=h();
+    if(g==0)return true;
+    if(dep+g>limit)return false;
+    if(dep==limit)return false;
+    vector<pair<int,int>>cand;
+    cand.reserve(n-1);
+    for(int k=2;k<=n;k++){
+        if(k==last)continue;
+        reverse(a+1,a+1+k);
+        int ng=h();
+        cand.push_back({ng,k});
+        reverse(a+1,a+1+k);
     }
-    int temp;
-    for(int i=2;i<=n;i++){
-        if(i==x||abs(a[i+1]-a[i])==1)continue;
-        temp=eva;
-        reverse(a+1,a+i+1);
-        if(abs(a[i]-a[i+1])==1)temp=eva-1;
-        if(temp+step<=maxstep){
-            dfs(i,temp,step+1);if(d)return;
-        }
-        reverse(a+1,a+i+1);
+    sort(cand.begin(),cand.end());
+    for(auto[ng,k]:cand){
+        reverse(a+1,a+1+k);
+        if(dfs(dep+1,limit,k))return true;
+        reverse(a+1,a+1+k);
     }
+    return false;
 }
 int main(){
-    rd(n);
-    for(int i=1;i<=n;i++){rd(a[i]);b[i]=a[i];}
-    a[n+1]=n+1;
-    sort(b+1,b+n+1);
-    for(int i=1;i<=n;++i)a[i]=lower_bound(b+1,b+n+1,a[i])-b;
-    for(maxstep=0;;maxstep++){
-        dfs(1,evaluate(),0);
-        if(d){cout<<maxstep;return 0;}
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin>>n;
+    vector<int>r(n+1),tmp;
+    for(int i=1;i<=n;i++){
+        cin>>r[i];
+        tmp.push_back(r[i]);
     }
+    sort(tmp.begin(),tmp.end());
+    for(int i=1;i<=n;i++)a[i]=lower_bound(tmp.begin(),tmp.end(),r[i])-tmp.begin()+1;
+    a[0]=0;
+    a[n+1]=n+1;
+    for(int limit=(h()+1)/2;;limit++){
+        if(dfs(0,limit,0)){
+            cout<<limit<<'\n';
+            break;
+        }
+    }
+    return 0;
 }
